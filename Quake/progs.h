464ac94869cc2b90dc065ba35a50b1da0ece2f23
temx <nukhead@gmail.com>
2022-06-24 19:13:06 +0200

    Increase number of area nodes for large maps
    ~9% faster average server tic in ad_tears
    ~17% faster average server tic in shib8
    >2x faster in 10kknights

--- Quake/progs.h
+++ Quake/progs.h
@@ -356,8 +356,8 @@ typedef struct areanode_s
 	link_t	trigger_edicts;
 	link_t	solid_edicts;
 } areanode_t;
-#define	AREA_DEPTH	4
-#define	AREA_NODES	32
+#define MAX_AREA_DEPTH 9
+#define AREA_NODES (2<<MAX_AREA_DEPTH)
 
 #define CSIE_KEYDOWN			0
 #define CSIE_KEYUP				1
