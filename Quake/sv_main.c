79de771ba01a674eb4796213a9b0a3ac89747369
temx <nukhead@gmail.com>
2023-02-07 01:24:13 +0100

    Fix FTE proto using stale alpha field for culling
    Fixes alpha-invisible entities (entities with 0<alpha<1/508) loaded from
    a savegame staying invisible after getting an increased alpha value.

--- Quake/sv_main.c
+++ Quake/sv_main.c
@@ -1422,7 +1422,7 @@ invisible:
 			ents[numents].state.velocity[2] = ent->v.velocity[2]*8;
 		}
 		/*TODO: other players *should* provide movetype+msec+v_angle+movement+velocity info so they can be extrapolated by fancy clients*/
-		else if (ent->alpha == ENTALPHA_ZERO && !ent->v.effects)	//don't send invisible entities unless they have effects
+		else if (ents[numents].state.alpha == ENTALPHA_ZERO && !ent->v.effects)	//don't send invisible entities unless they have effects
 			continue;
 		val = GetEdictFieldValue(ent, qcvm->extfields.exteriormodeltoclient);
 		if (val && val->edict == proged)
