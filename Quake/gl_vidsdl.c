1e6bec6b9f5c9ae5570690bc3d489bd4fda91b60
Ozkan Sezer <sezeroz@gmail.com>
2023-09-02 23:55:50 +0300

    Revert the depth_clamp extension patch.
    Caused regression with the Re:Mobilize mod. See
    https://github.com/sezero/quakespasm/issues/78.
    
    (cherry picked from commit 7c993461f2bec487b2fe1bdcb4f88abdf987197d)

--- Quake/gl_vidsdl.c
+++ Quake/gl_vidsdl.c
@@ -121,7 +121,6 @@ modestate_t	modestate = MS_UNINIT;
 qboolean	scr_skipupdate;
 
 qboolean gl_mtexable = false;
-qboolean gl_nv_depth_clamp = false;
 qboolean gl_texture_env_combine = false; //johnfitz
 qboolean gl_texture_env_add = false; //johnfitz
 qboolean gl_swap_control = false; //johnfitz
@@ -1512,27 +1511,6 @@ static void GL_CheckExtensions (void)
 	{
 		Con_Warning ("GLSL alias model rendering not available, using Fitz renderer\n");
 	}
-
-	// ARB_depth_clamp
-	//
-	if (COM_CheckParm("-nodepthclamp"))
-		Con_Warning ("depth_clamp disabled at command line\n");
-	else if (GL_ParseExtensionList(gl_extensions, "GL_ARB_depth_clamp"))
-	{
-		if (cls.state == ca_disconnected) // woods #supressvidmsgs
-			Con_Printf("FOUND: ARB_depth_clamp\n");
-		gl_nv_depth_clamp = true;
-	}
-	else if (GL_ParseExtensionList(gl_extensions, "GL_NV_depth_clamp"))
-	{
-		if (cls.state == ca_disconnected) // woods #supressvidmsgs
-			Con_Printf("FOUND: GL_NV_depth_clamp\n");
-		gl_nv_depth_clamp = true;
-	}
-	else
-	{
-		Con_Warning ("depth_clamp not supported\n");
-	}
 	// glGenerateMipmap for warp textures
 	if (COM_CheckParm("-nowarpmipmaps"))
 		Con_Warning ("glGenerateMipmap disabled at command line\n");
@@ -1588,8 +1566,6 @@ static void GL_SetupState (void)
 	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glDepthRange (0, 1); //johnfitz -- moved here becuase gl_ztrick is gone.
 	glDepthFunc (GL_LEQUAL); //johnfitz -- moved here becuase gl_ztrick is gone.
-	if (gl_nv_depth_clamp)
-	    glEnable(GL_DEPTH_CLAMP_NV);
 }
 
 /*
