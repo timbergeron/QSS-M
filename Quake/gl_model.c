435dca37acbf11453d66a4a319905c3bb81b2d97
Andrei Drexler <makro@rq3.com>
2024-03-09 17:48:35 +0100

    Fix potential buffer overflow in Mod_LoadAliasFrame
    e.g. frame #1 (`Extend Tentacles`) of `progs/tarbaby.mdl`
    from LibreQuake (https://github.com/MissLavender-LQ/LibreQuake/blob/bbe8e5a0850600ba774a34d5ba58817c2fd460f6/lq1/progs/tarbaby.mdl)
    doesn't contain a NUL terminator.
    
    (cherry picked from commit 470ee966dcdf017d41f3562f62eac8e417f230ab)

--- Quake/gl_model.c
+++ Quake/gl_model.c
@@ -3739,7 +3739,7 @@ static void *Mod_LoadAliasFrame (void * pin, maliasframedesc_t *frame, int pvtyp
 
 	pdaliasframe = (daliasframe_t *)pin;
 
-	strcpy (frame->name, pdaliasframe->name);
+	q_strlcpy (frame->name, pdaliasframe->name, sizeof (frame->name));
 	frame->firstpose = posenum;
 	frame->numposes = 1;
 
