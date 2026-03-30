sezero/quakespasm

Revise glGenerateMipmap detection:
Link to glGenerateMipmap if gl_version >= 3 or GL_ARB_framebuffer_object
is present. Fallback to glGenerateMipmapEXT if GL_EXT_framebuffer_object
is present.

Quake/gl_vidsdl.c
@@ -1303,10 +1303,19 @@ static void GL_CheckExtensions (void)
 		Con_Warning ("glGenerateMipmap disabled at command line\n");
 	else
 	{
-		GL_GenerateMipmap = (QS_PFNGENERATEMIPMAP) SDL_GL_GetProcAddress("glGenerateMipmap");
-		if (GL_GenerateMipmap != NULL)
-			Con_Printf ("FOUND: glGenerateMipmap\n");
-		else
+		if (gl_version_major >= 3 || GL_ParseExtensionList(gl_extensions, "GL_ARB_framebuffer_object"))
+		{
+			GL_GenerateMipmap = (QS_PFNGENERATEMIPMAP) SDL_GL_GetProcAddress("glGenerateMipmap");
+			if (GL_GenerateMipmap != NULL)
+				Con_Printf ("FOUND: glGenerateMipmap\n");
+		}
+		else if (GL_ParseExtensionList(gl_extensions, "GL_EXT_framebuffer_object"))
+		{
+			GL_GenerateMipmap = (QS_PFNGENERATEMIPMAP) SDL_GL_GetProcAddress("glGenerateMipmapEXT");
+			if (GL_GenerateMipmap != NULL)
+				Con_Printf ("FOUND: glGenerateMipmapEXT\n");
+		}
+		if (GL_GenerateMipmap == NULL)
 			Con_Warning ("glGenerateMipmap not available, liquids won't have mipmaps\n");
 	}
 }

timbergeron/QSS-M

(cherry picked from commit 87ebe3aa8efb08fd3b33a174e6bae25a91a1e557)

Quake/gl_vidsdl.c
@@ -1516,13 +1516,25 @@ static void GL_CheckExtensions (void)
 		Con_Warning ("glGenerateMipmap disabled at command line\n");
 	else
 	{
-		GL_GenerateMipmap = (QS_PFNGENERATEMIPMAP) SDL_GL_GetProcAddress("glGenerateMipmap");
-		if (GL_GenerateMipmap != NULL)
+		if (gl_version_major >= 3 || GL_ParseExtensionList(gl_extensions, "GL_ARB_framebuffer_object"))
 		{
-			if (cls.state == ca_disconnected) // woods #supressvidmsgs
-				Con_Printf("FOUND: glGenerateMipmap\n");
+			GL_GenerateMipmap = (QS_PFNGENERATEMIPMAP) SDL_GL_GetProcAddress("glGenerateMipmap");
+			if (GL_GenerateMipmap != NULL)
+			{
+				if (cls.state == ca_disconnected) // woods #supressvidmsgs
+					Con_Printf("FOUND: glGenerateMipmap\n");
+			}
 		}
-		else
+		else if (GL_ParseExtensionList(gl_extensions, "GL_EXT_framebuffer_object"))
+		{
+			GL_GenerateMipmap = (QS_PFNGENERATEMIPMAP) SDL_GL_GetProcAddress("glGenerateMipmapEXT");
+			if (GL_GenerateMipmap != NULL)
+			{
+				if (cls.state == ca_disconnected) // woods #supressvidmsgs
+					Con_Printf("FOUND: glGenerateMipmapEXT\n");
+			}
+		}
+		if (GL_GenerateMipmap == NULL)
 			Con_Warning ("glGenerateMipmap not available, liquids won't have mipmaps\n");
 	}
 }