sezero/quakespasm

Search extension from the end of map name

Quake/host_cmd.c
@@ -858,9 +858,10 @@ static void Host_Map_f (void)
 	svs.serverflags = 0;			// haven't completed an episode yet
 	q_strlcpy (name, Cmd_Argv(1), sizeof(name));
 	// remove (any) trailing ".bsp" from mapname -- S.A.
-	p = strstr(name, ".bsp");
-	if (p && p[4] == '\0')
-		*p = '\0';
+	p = Q_strrchr(name, '.');
+	if (p != NULL)
+		if (Q_strcmp(p, ".bsp") == 0)
+			*p = '\0';
 	SV_SpawnServer (name);
 	if (!sv.active)
 		return;

timbergeron/QSS-M

Quake/host_cmd.c
@@ -5358,9 +5358,12 @@ static void Host_Map_f (void)
 	svs.serverflags = 0;			// haven't completed an episode yet
 	q_strlcpy (name, Cmd_Argv(1), sizeof(name));
 	// remove (any) trailing ".bsp" from mapname -- S.A.
-	p = strstr(name, ".bsp");
-	if (p && p[4] == '\0')
-		*p = '\0';
+	p = strrchr(name, '.');
+	if (p != NULL)
+	{
+		if (strcmp(p, ".bsp") == 0)
+			*p = '\0';
+	}
 
 	if (cls.state != ca_dedicated) // woods -- try to download map
 	{