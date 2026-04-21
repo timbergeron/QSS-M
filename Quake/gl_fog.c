j0zzz/JoeQuake

Fog parameters are properly normalized at world spawn
Bug occured on map bm_chrisholden

trunk/gl_fog.c
@@ -217,6 +217,10 @@ void Fog_ParseWorldspawn (void)
 		if (!strcmp("fog", key))
 		{
 			sscanf(value, "%f %f %f %f", &fog_density, &fog_red, &fog_green, &fog_blue);
+			fog_density = max(0.0, fog_density);
+			fog_red = bound(0.0, fog_red, 1.0);
+			fog_green = bound(0.0, fog_green, 1.0);
+			fog_blue = bound(0.0, fog_blue, 1.0);
 		}
 	}
 }

timbergeron/QSS-M

(cherry picked from commit 95e14008b7f2bbd5061dfc1b3942541b7b1c3ba2)

Quake/gl_fog.c
@@ -232,6 +232,10 @@ void Fog_ParseWorldspawn (void)
 		if (!strcmp("fog", key))
 		{
 			sscanf(value, "%f %f %f %f", &fog_density, &fog_red, &fog_green, &fog_blue);
+			fog_density = q_max(0.0, fog_density);
+			fog_red = bound(0.0, fog_red, 1.0);
+			fog_green = bound(0.0, fog_green, 1.0);
+			fog_blue = bound(0.0, fog_blue, 1.0);
 		}
 	}
 }