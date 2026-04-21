j0zzz/JoeQuake

Add cvar to disable attempts to get domain names
On each connection to a server, there is an attempt to find a domain
name for a server from its IP address using a DNS lookup. On Windows,
this can take a long time though. Disabling it can speedup connecting to
servers significantly. As I struggle to see any use case for domain
names, the lookups are disabled by default.

trunk/net_main.c
@@ -65,6 +65,7 @@ int unreliableMessagesReceived = 0;
 cvar_t	net_messagetimeout = {"net_messagetimeout", "300"};
 cvar_t	net_connectsearch = {"net_connectsearch", "1"};
 cvar_t	net_connecttimeout = {"net_connecttimeout", "10"};	// joe: qkick/qflood protection from ProQuake
+cvar_t	net_getdomainname = {"net_getdomainname", "0"};
 cvar_t	hostname = {"hostname", "UNNAMED"};
 cvar_t	cl_password = {"cl_password", ""};		// joe: password protection from ProQuake
 cvar_t	rcon_password = {"rcon_password", ""};		// joe: rcon password from ProQuake
@@ -914,6 +915,7 @@ void NET_Init (void)
 	Cvar_Register (&net_messagetimeout);
 	Cvar_Register (&net_connectsearch);
 	Cvar_Register (&net_connecttimeout);	// joe: qkick/qflood protection from ProQuake
+	Cvar_Register (&net_getdomainname);
 	Cvar_Register (&hostname);
 	Cvar_Register (&cl_password);		// joe: password protection from ProQuake
 	Cvar_Register (&rcon_password);		// joe: rcon password from ProQuake

timbergeron/QSS-M

On each connection to a server, there is an attempt to find a domain name for a server from its IP address using a DNS lookup. On Windows, this can take a long time though. Disabling it can speedup connecting to servers significantly. As I struggle to see any use case for domain names, the lookups are disabled by default.
(cherry picked from commit 89ceb20850e960f2947a1571914e4b77188751c2)

Quake/net_main.c
@@ -68,6 +68,7 @@ int		unreliableMessagesReceived	= 0;
 cvar_t	net_messagetimeout = {"net_messagetimeout","300",CVAR_NONE};
 cvar_t	net_connectsearch = {"net_connectsearch","1",CVAR_NONE};
 cvar_t	net_connecttimeout = {"net_connecttimeout","10",CVAR_NONE};	//this might be a little brief, but we don't have a way to protect against smurf attacks.
+cvar_t	net_getdomainname = {"net_getdomainname","0",CVAR_NONE};
 cvar_t	net_connectattempts = {"net_connectattempts","3",CVAR_ARCHIVE}; // woods #connectretry
 cvar_t	hostname = {"hostname", "UNNAMED", CVAR_SERVERINFO};
 cvar_t	net_master_ignore = {"net_master_ignore", "159.196.122.212", CVAR_ARCHIVE};
@@ -1215,6 +1216,7 @@ void NET_Init (void)
 	Cvar_RegisterVariable (&net_messagetimeout);
 	Cvar_RegisterVariable (&net_connectsearch);
 	Cvar_RegisterVariable (&net_connecttimeout);
+	Cvar_RegisterVariable (&net_getdomainname);
 	Cvar_RegisterVariable (&net_connectattempts); // woods #connectretry
 	Cvar_RegisterVariable (&hostname);
 	Cvar_RegisterVariable (&net_master_ignore);