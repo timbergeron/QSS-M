matthewearl/quakespasm

Prevent 0-sized textures when using gl_picmip

Quake/gl_texmgr.c
@@ -743,7 +743,7 @@ int TexMgr_SafeTextureSize (int s)
 {
 	if ((int)gl_max_size.value > 0)
 		s = q_min(TexMgr_Pad((int)gl_max_size.value), s);
-	s = q_min(gl_max_texture_size, s);
+	s = CLAMP(1, s, gl_max_texture_size);
 	return s;
 }
 

timbergeron/QSS-M

(cherry picked from commit b9d2a9c4f3049515b3740801ae8973f6b4f444fa)

Quake/gl_texmgr.c
@@ -947,8 +947,7 @@ int TexMgr_SafeTextureSize (int s)
 		p = TexMgr_Pad(p);
 		if (p < s) s = p;
 	}
-	if (s > gl_hardware_maxsize)
-	    s = gl_hardware_maxsize;
+	s = CLAMP(1, s, gl_hardware_maxsize);
 	return s;
 }
 
@@ -966,8 +965,7 @@ int TexMgr_SafeTextureSize2 (int s)
 		p = TexMgr_Pad(p);
 		if (p < s) s = p;
 	}
-	if (s > gl_hardware_maxsize)
-		s = gl_hardware_maxsize;
+	s = CLAMP(1, s, gl_hardware_maxsize);
 	return s;
 }
 
@@ -985,8 +983,7 @@ int TexMgr_SafeTextureSize3 (int s)
 		p = TexMgr_Pad(p);
 		if (p < s) s = p;
 	}
-	if (s > gl_hardware_maxsize)
-		s = gl_hardware_maxsize;
+	s = CLAMP(1, s, gl_hardware_maxsize);
 	return s;
 }
 