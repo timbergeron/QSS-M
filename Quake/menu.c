Novum/vkQuake

Bound menu and local server strings

Quake/menu.c
@@ -1282,8 +1282,8 @@ static void M_Menu_Setup_f (void)
 	key_dest = key_menu;
 	m_state = m_setup;
 	m_entersound = true;
-	strcpy (setup_myname, cl_name.string);
-	strcpy (setup_hostname, hostname.string);
+	q_strlcpy (setup_myname, cl_name.string, sizeof (setup_myname));
+	q_strlcpy (setup_hostname, hostname.string, sizeof (setup_hostname));
 	setup_top = setup_oldtop = ((int)cl_topcolor.value);
 	setup_bottom = setup_oldbottom = ((int)cl_bottomcolor.value);
 }

Quake/net_loop.c
@@ -48,14 +48,14 @@ qboolean Loop_SearchForHosts (qboolean xmit)
 
 	hostCacheCount = 1;
 	if (strcmp (hostname.string, "UNNAMED") == 0)
-		strcpy (hostcache[0].name, "local");
+		q_strlcpy (hostcache[0].name, "local", sizeof (hostcache[0].name));
 	else
-		strcpy (hostcache[0].name, hostname.string);
-	strcpy (hostcache[0].map, sv.name);
+		q_strlcpy (hostcache[0].name, hostname.string, sizeof (hostcache[0].name));
+	q_strlcpy (hostcache[0].map, sv.name, sizeof (hostcache[0].map));
 	hostcache[0].users = net_activeconnections;
 	hostcache[0].maxusers = svs.maxclients;
 	hostcache[0].driver = net_driverlevel;
-	strcpy (hostcache[0].cname, "local");
+	q_strlcpy (hostcache[0].cname, "local", sizeof (hostcache[0].cname));
 	return false;
 }
 

timbergeron/QSS-M

Quake/menu.c
@@ -8773,8 +8773,8 @@ void M_Menu_Setup_f (void)
 	if (from_namemaker) // woods #namemaker
 		from_namemaker = !from_namemaker;
 	else
-		Q_strcpy(setup_myname, cl_name.string);
-	Q_strcpy(setup_hostname, hostname.string);
+		q_strlcpy(setup_myname, cl_name.string, sizeof(setup_myname));
+	q_strlcpy(setup_hostname, hostname.string, sizeof(setup_hostname));
 	setup_top = setup_oldtop = CL_PLColours_Parse(cl_topcolor.string);
 	setup_bottom = setup_oldbottom = CL_PLColours_Parse(cl_bottomcolor.string);
 	M_TextField_Init(&setup_hostname_field, setup_hostname, 15, false);

Quake/net_loop.c
@@ -55,14 +55,14 @@ qboolean Loop_SearchForHosts (qboolean xmit)
 
 	hostCacheCount = 1;
 	if (Q_strcmp(hostname.string, "UNNAMED") == 0)
-		Q_strcpy(hostcache[0].name, "local");
+		q_strlcpy(hostcache[0].name, "local", sizeof(hostcache[0].name));
 	else
-		Q_strcpy(hostcache[0].name, hostname.string);
-	Q_strcpy(hostcache[0].map, sv.name);
+		q_strlcpy(hostcache[0].name, hostname.string, sizeof(hostcache[0].name));
+	q_strlcpy(hostcache[0].map, sv.name, sizeof(hostcache[0].map));
 	hostcache[0].users = net_activeconnections;
 	hostcache[0].maxusers = svs.maxclients;
 	hostcache[0].driver = net_driverlevel;
-	Q_strcpy(hostcache[0].cname, "local");
+	q_strlcpy(hostcache[0].cname, "local", sizeof(hostcache[0].cname));
 	return false;
 }
 